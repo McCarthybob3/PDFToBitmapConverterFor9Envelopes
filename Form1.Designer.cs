4ebd5d36da0af0bdd26573dfc7d14cc5b3df7801
McCarthybob3 <mccarthybob3@gmail.com>
2021-09-27 22:02:50 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace PDFtoBitmapEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(667, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 93);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(616, 623);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(667, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 102);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save file";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(667, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 116);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "edit";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(821, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(632, 623);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(667, 498);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 104);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "multiple";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(667, 608);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(134, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1611, 647);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFtoBitmapEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //VaryQualityLevel((Bitmap)pictureBox1.Image);
+        }
+
+        //edit picture
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+            button2.Enabled = true;
+
+            ConvertPDF();
+
+
+        }
+
+        //save edited single image
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            SaveImageCustomPath(pictureBox2.Image);
+
+                //string path = Path.Combine(saveFileDialog1, "newimg.jpg");
+
+                //SaveEdit(pictureBox2.Image, path);
+            
+        }
+
+        //open specific file
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var test = CreateBitmapAtRuntime((Bitmap)pictureBox1.Image);
+            pictureBox2.Image = test;
+        }
+
+        private void SaveEdit(Image edit, String path)
+        {
+            edit.Save(path);
+        }
+
+    private Bitmap CreateBitmapAtRuntime(Bitmap edit)
+        {
+            Bitmap img = edit;
+            Bitmap newImage;
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            newImage = img;
+            Graphics gr = Graphics.FromImage(newImage);
+
+            gr.DrawImageUnscaled(img, new Rectangle(160, 0, 200, 200));
+          //  gr.DrawImageUnscaledAndClipped(img, new Rectangle(30, 0, 200, 200));
+            return newImage;
+        }
+
+        private void ConvertPDF()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                var fileStream = openFileDialog.OpenFile();
+
+                using (var pdfDocument = PdfiumViewer.PdfDocument.Load(@filePath))
+                {
+                   
+                    var bitmapImage = pdfDocument.Render(0, 1000, 1000, PdfRenderFlags.CorrectFromDpi);
+                    // bitmapImage.Save(@"image.bmp", ImageFormat.Bmp);
+                    /////   pictureBox1.Size = bitmapImage.Size;
+                    /////  pictureBox1.Image = bitmapImage;
+                    pictureBox1.Image = bitmapImage;
+                }
+            }
+        }
+
+
+        private Image ConvertPDF(string filePath)
+        {
+       
+                using (var pdfDocument = PdfiumViewer.PdfDocument.Load(@filePath))
+                {
+                    var bitmapImage = pdfDocument.Render(0, 1000, 1000, PdfRenderFlags.CorrectFromDpi);
+                    // bitmapImage.Save(@"image.bmp", ImageFormat.Bmp);
+                    /////   pictureBox1.Size = bitmapImage.Size;
+                    /////  pictureBox1.Image = bitmapImage;
+                    return bitmapImage;
+                }
+        
+        }
+
+        //edit multiple
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var progress = 0;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.DefaultExt = System.Drawing.Imaging.ImageFormat.Jpeg.ToString();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            // saveFileDialog1.Filter = "Tiff Image|*.tiff";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.FileName = "new.jpg";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    var directory = Path.GetDirectoryName(openFileDialog.FileName);
+
+                    string[] files = Directory.GetFiles(directory, "*.pdf");
+
+                    progressBar1.Maximum = files.Count();
+
+                    foreach (var file in files)
+                    {
+                        Image test = ConvertPDF(file);
+                        var newimage = CreateBitmapAtRuntime((Bitmap)test);
+                        SaveImageCustomPath(newimage, saveFileDialog1.FileName);
+                        progress++;
+                        progressBar1.Value = progress;
+                    }
+                }
+            }
+        }
+
+
+        private void SaveImageCustomPath(Image edit)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.DefaultExt = System.Drawing.Imaging.ImageFormat.Jpeg.ToString();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.FileName = "new.jpg";
+
+
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                if (saveFileDialog1.FileName != "")
+                {
+                    System.IO.FileStream fs =
+                        (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            edit.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+
+                        case 2:
+                            edit.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+
+                        case 3:
+                            edit.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                    }
+
+                    //string path = Path.Combine(saveFileDialog1, "newimg.jpg");
+
+                    //SaveEdit(pictureBox2.Image, path);
+                }
+            }
+
+        }
+
+        private void SaveImageCustomPath(Bitmap edit, String path)
+        {
+
+            var count = 0;
+
+            var newpath = path;
+
+            while (System.IO.File.Exists(newpath))
+            {
+                count++;
+               newpath =  Path.GetDirectoryName(path)
+                     + Path.DirectorySeparatorChar
+                     + Path.GetFileNameWithoutExtension(path)
+                     + count.ToString()
+                     + Path.GetExtension(path);
+            }
+
+
+            VaryQualityLevel(edit, newpath);
+            //edit.Save(newpath,System.Drawing.Imaging.ImageFormat.Tiff);
+               
+
+            }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void VaryQualityLevel(Bitmap bmp, String path)
+        {
+            using (bmp)
+            {
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                System.Drawing.Imaging.Encoder myEncoder =
+                    System.Drawing.Imaging.Encoder.Quality;
+
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+
+                myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                bmp.Save(path, jpgEncoder, myEncoderParameters);
+
+                // zero compression
+                //myEncoderParameter = new EncoderParameter(myEncoder, 0L);
+                //myEncoderParameters.Param[0] = myEncoderParameter;
+                //bmp.Save(path, jpgEncoder, myEncoderParameters);
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+
+    }
+    }
+
